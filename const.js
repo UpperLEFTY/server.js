b913b3889a57a52e1b6db2827e9ca486cd89f785
Essence Alexander <upperlefty@zoho.eu>
2018-03-02 10:23:38 -0800

    Update const.js

--- const.js
+++ const.js
@@ -7,6 +7,22 @@ const favicon = require('serve-favicon')
 const compression = require('compression')
 const resolve = file => path.resolve(__dirname, file)
 const { createBundleRenderer } = require('vue-server-renderer')
+const fs = require('fs')
+const path = require('path')
+const LRU = require('lru-cache')
+const express = require('express')
+
+var cookieParser = require('cookie-parser');
+const favicon = require('serve-favicon')
+const compression = require('compression')
+const resolve = file => path.resolve(__dirname, file)
+const { createBundleRenderer } = require('vue-server-renderer')
 
 
+
+const isProd = process.env.NODE_ENV === 'production'
+const useMicroCache = process.env.MICRO_CACHE !== 'false'
+const serverInfo =
+  `express/${require('express/package.json').version} ` +
+  `vue-server-renderer/${require('vue-server-renderer/package.json').version}`
 
